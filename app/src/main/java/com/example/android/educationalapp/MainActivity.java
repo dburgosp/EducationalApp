d00e445ab0a0cbb86a078b3e04fa7318c0b69f25
David Burgos Prieto <david.burgos.prieto@gmail.com>
2017-04-05 20:21:15 +0200

    Intento de salvar el estado para que el vídeo no se reinicie al girar el móvil... Hay que seguir afinando.

--- app/src/main/java/com/example/android/educationalapp/MainActivity.java
+++ app/src/main/java/com/example/android/educationalapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.educationalapp;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,12 @@ import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
     VideoView videoView;
+    int currentVideoPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Uri uri;
 
         // Hide title bar.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -20,18 +23,34 @@ public class MainActivity extends AppCompatActivity {
         //getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+        //Toast.makeText(this, "Tap screen to skip intro", Toast.LENGTH_LONG);
 
         // Play intro video.
+        if (savedInstanceState != null) {
+            currentVideoPosition = savedInstanceState.getInt("currentVideoPosition");
+        }
         videoView = (VideoView) findViewById(R.id.video_view);
-        Uri uri = Uri.parse("android.resource://" + this.getPackageName() + "/" + R.raw.intro_es);
+        uri = Uri.parse("android.resource://" + this.getPackageName() + "/" + R.raw.intro_es);
+        //videoView.setOnPreparedListener(this);
+        videoView.setKeepScreenOn(true);
         videoView.setVideoURI(uri);
         videoView.start();
+
+        // When intro video ends, show activity_main_menu.
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                setContentView(R.layout.activity_main_menu);
+            }
+        });
     }
+
 
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putInt("currentVideoPosition", videoView.getCurrentPosition());
         /*
         outState.putInt("goalsTeamA", goalsTeamA);
         outState.putInt("goalsTeamB", goalsTeamB);
@@ -45,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        currentVideoPosition=savedInstanceState.getInt("currentVideoPosition");
         /*
         goalsTeamA = savedInstanceState.getInt("goalsTeamA");
         goalsTeamB = savedInstanceState.getInt("goalsTeamB");
